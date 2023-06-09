2fb2e7cdc23108f88a33708688de6dd62ad118a2
Duane Johnson <duane.johnson@gmail.com>
2023-06-08 22:31:10 -0600

    Export as module

--- index.js
+++ index.js
@@ -1,195 +1,229 @@
+export const shelf = {};
 
-var shelf = {}
+shelf.wrap = (s) =>
+  is_obj(s)
+    ? Object.fromEntries(Object.entries(s).map(([k, v]) => [k, shelf.wrap(v)]))
+    : [s];
 
-;(() => {
-    function is_obj(o) { return o && typeof o == 'object' && !Array.isArray(o) }
-    function greater_than(a, b) {
-        if (is_obj(b)) return false
-        if (is_obj(a)) return true
-        return JSON.stringify(a) > JSON.stringify(b)
+shelf.create = (init) => {
+  let x = [null, -1];
+  if (init !== undefined) shelf.merge(x, shelf.wrap(init));
+  return x;
+};
+
+shelf.read = (s, ...path) => {
+  s = path.reduce((s, x) => s?.[0]?.[x], s);
+  if (s && is_obj(s[0])) {
+    return Object.fromEntries(
+      Object.entries(s[0])
+        .map(([k, v]) => [k, shelf.read(v)])
+        .filter(([k, v]) => v != null)
+    );
+  } else return s?.[0];
+};
+shelf.get = shelf.read;
+
+shelf.get_change = (a, b) => {
+  return shelf.merge(a, b, true);
+};
+
+shelf.merge = (a, b, dont_modify) => {
+  let change = null;
+
+  if (!a) a = [null, -1];
+  if (!Array.isArray(b)) b = [b];
+
+  let both_objs = is_obj(a[0]) && is_obj(b[0]);
+  let eq = equal(a[0], b[0]);
+
+  if (b[1] == null) b = [b[0], a[1] + (eq ? 0 : 1)];
+  else if (b[1] == "add") b = [a[0] + b[0], a[1] + 1];
+
+  if (b[1] > (a[1] ?? -1) || (b[1] == a[1] && greater_than(b[0], a[0]))) {
+    if (is_obj(b[0])) {
+      if (!dont_modify) {
+        a[0] = {};
+        a[1] = b[1];
+      }
+      change = shelf.merge(dont_modify ? [{}, b[1]] : a, b, dont_modify);
+      if (!change) change = [{}, b[1]];
+    } else {
+      if (!dont_modify) {
+        a[0] = b[0];
+        a[1] = b[1];
+      }
+      change = b;
     }
-    function equal(a, b) {
-        if (is_obj(a)) return is_obj(b)
-        if (is_obj(b)) return false
-        return JSON.stringify(a) == JSON.stringify(b)
+  } else if (b[1] == a[1] && both_objs) {
+    for (let [k, v] of Object.entries(b[0])) {
+      if (!dont_modify && !a[0][k]) a[0][k] = [null, -1];
+      let diff = shelf.merge(a[0][k], v, dont_modify);
+      if (diff) {
+        if (!change) change = [{}, b[1]];
+        change[0][k] = diff;
+      }
     }
+  }
+  return change;
+};
 
-    shelf.wrap = s => is_obj(s) ? Object.fromEntries(Object.entries(s).map(([k, v]) => [k, shelf.wrap(v)])) : [s]
+shelf.mask = (s, mask) => {
+  return mask == true || !is_obj(s[0])
+    ? s
+    : [
+        Object.fromEntries(
+          Object.entries(mask)
+            .filter(([k, v]) => s[0][k])
+            .map(([k, v]) => [k, shelf.mask(s[0][k], v)])
+        ),
+        s[1],
+      ];
+};
 
-    shelf.create = init => {
-        let x = [null, -1]
-        if (init !== undefined) shelf.merge(x, shelf.wrap(init))
-        return x            
-    }
-    
-    shelf.read = (s, ...path) => {
-        s = path.reduce((s, x) => s?.[0]?.[x], s)
-        if (s && is_obj(s[0])) {
-            return Object.fromEntries(Object.entries(s[0]).map(([k, v]) => [k, shelf.read(v)]).filter(([k, v]) => v != null))
-        } else return s?.[0]
-    }
-    shelf.get = shelf.read
+shelf.proxy = (s, cb) => {
+  return new Proxy(s[0], {
+    get(o, k) {
+      if (k == Symbol.toPrimitive) return () => null;
+      let x = o[k]?.[0];
+      if (x && typeof x == "object" && !Array.isArray(x)) {
+        return shelf.proxy(o[k], (delta) => cb([{ [k]: delta }, s[1]]));
+      } else return x;
+    },
+    set(o, k, v) {
+      let x = shelf.merge(s, { [k]: v });
+      if (x) cb(x);
+      return true;
+    },
+    deleteProperty(o, k) {
+      let x = shelf.merge(s, { [k]: null });
+      if (x) cb(x);
+      return true;
+    },
+    ownKeys(o) {
+      return Reflect.ownKeys(o).filter((k) => o[k][0] != null);
+    },
+  });
+};
 
-    shelf.get_change = (a, b) => {
-        return shelf.merge(a, b, true)
-    }
-
-    shelf.merge = (a, b, dont_modify) => {
-        let change = null
-
-        if (!a) a = [null, -1]
-        if (!Array.isArray(b)) b = [b]
-
-        let both_objs = is_obj(a[0]) && is_obj(b[0])
-        let eq = equal(a[0], b[0])
-
-        if (b[1] == null) b = [b[0], a[1] + (eq ? 0 : 1)]
-        else if (b[1] == 'add') b = [a[0] + b[0], a[1] + 1]
-
-        if (b[1] > (a[1] ?? -1) || (b[1] == a[1] && greater_than(b[0], a[0]))) {
-            if (is_obj(b[0])) {
-                if (!dont_modify) {
-                    a[0] = {}
-                    a[1] = b[1]
-                }
-                change = shelf.merge(dont_modify ? [{}, b[1]] : a, b, dont_modify)
-                if (!change) change = [{}, b[1]]
-            } else {
-                if (!dont_modify) {
-                    a[0] = b[0]
-                    a[1] = b[1]
-                }
-                change = b
-            }
-        } else if (b[1] == a[1] && both_objs) {
-            for (let [k, v] of Object.entries(b[0])) {
-                if (!dont_modify && !a[0][k]) a[0][k] = [null, -1]
-                let diff = shelf.merge(a[0][k], v, dont_modify)
-                if (diff) {
-                    if (!change) change = [{}, b[1]]
-                    change[0][k] = diff
-                }
-            }
+shelf.local_update = (backend, frontend, override_new_version) => {
+  if (equal(backend[0], frontend)) {
+    if (is_obj(frontend)) {
+      var ret = [{}, backend[1]];
+      for (let [k, v] of Object.entries(backend[0])) {
+        if (v[0] != null && frontend[k] == null) {
+          v[0] = null;
+          v[1] = override_new_version || (v[1] ?? -1) + 1;
+          ret[0][k] = v;
         }
-        return change
+      }
+      for (let [k, v] of Object.entries(frontend)) {
+        if (!backend[0][k]) backend[0][k] = [null, -1];
+        let changes = shelf.local_update(
+          backend[0][k],
+          v,
+          override_new_version
+        );
+        if (changes) ret[0][k] = changes;
+      }
+      return Object.keys(ret[0]).length ? ret : null;
     }
-    
-    shelf.mask = (s, mask) => {
-        return mask == true || !is_obj(s[0]) ? s : [Object.fromEntries(Object.entries(mask).filter(([k, v]) => s[0][k]).map(([k, v]) => [k, shelf.mask(s[0][k], v)])), s[1]]
-    }
-
-    shelf.proxy = (s, cb) => {
-        return new Proxy(s[0], {
-            get(o, k) {
-                if (k == Symbol.toPrimitive) return () => null
-                let x = o[k]?.[0]
-                if (x && typeof(x) == 'object' && !Array.isArray(x)) {
-                    return shelf.proxy(o[k], delta => cb([{[k]: delta}, s[1]]))
-                } else return x
-            },
-            set(o, k, v) {
-                let x = shelf.merge(s, {[k]: v})
-                if (x) cb(x)
-                return true
-            },
-            deleteProperty(o, k) {
-                let x = shelf.merge(s, {[k]: null})
-                if (x) cb(x)
-                return true
-            },
-            ownKeys(o) { return Reflect.ownKeys(o).filter(k => o[k][0] != null) }
-        })
-    }
-    
-    shelf.local_update = (backend, frontend, override_new_version) => {
-        if (equal(backend[0], frontend)) {
-            if (is_obj(frontend)) {
-                var ret = [{}, backend[1]]
-                for (let [k, v] of Object.entries(backend[0])) {
-                    if (v[0] != null && frontend[k] == null) {
-                        v[0] = null
-                        v[1] = override_new_version || ((v[1] ?? -1) + 1)
-                        ret[0][k] = v
-                    }
-                }
-                for (let [k, v] of Object.entries(frontend)) {
-                    if (!backend[0][k]) backend[0][k] = [null, -1]
-                    let changes = shelf.local_update(backend[0][k], v, override_new_version)
-                    if (changes) ret[0][k] = changes
-                }
-                return Object.keys(ret[0]).length ? ret : null
-            }
+  } else {
+    backend[1] = override_new_version || (backend[1] ?? -1) + 1;
+    if (is_obj(frontend)) {
+      backend[0] = {};
+      for (let [k, v] of Object.entries(frontend)) {
+        if (is_obj(v)) {
+          backend[0][k] = [null, -1];
+          shelf.local_update(backend[0][k], v, override_new_version);
         } else {
-            backend[1] = override_new_version || ((backend[1] ?? -1) + 1)
-            if (is_obj(frontend)) {
-                backend[0] = {}
-                for (let [k, v] of Object.entries(frontend)) {
-                    if (is_obj(v)) {
-                        backend[0][k] = [null, -1]
-                        shelf.local_update(backend[0][k], v, override_new_version)
-                    } else {
-                        backend[0][k] = [v, 0]
-                    }
-                }
-            } else backend[0] = frontend
-            return backend
+          backend[0][k] = [v, 0];
         }
+      }
+    } else backend[0] = frontend;
+    return backend;
+  }
+};
+
+shelf.remote_update = (a, f, b) => {
+  if (b[1] > (a[1] ?? -1) || (b[1] == a[1] && greater_than(b[0], a[0]))) {
+    a[1] = b[1];
+    if (!is_obj(a[0]) && is_obj(f) && is_obj(b[0])) {
+      a[0] = {};
+      return shelf.remote_update(a, f, b);
     }
+    if (is_obj(b[0])) {
+      a[0] = {};
+      shelf.merge(a, b);
+    } else a[0] = b[0];
+    f = shelf.read(a);
+  } else if (b[1] == a[1] && is_obj(a[0]) && is_obj(b[0])) {
+    if (is_obj(f)) {
+      for (let [k, v] of Object.entries(b[0])) {
+        if (!a[0][k]) a[0][k] = [null, -1];
+        f[k] = shelf.remote_update(a[0][k], f[k], v);
+        if (f[k] == null) delete f[k];
+      }
+    } else shelf.merge(a, b);
+  }
+  return f;
+};
 
-    shelf.remote_update = (a, f, b) => {
-        if (b[1] > (a[1] ?? -1) || (b[1] == a[1] && greater_than(b[0], a[0]))) {
-            a[1] = b[1]
-            if (!is_obj(a[0]) && is_obj(f) && is_obj(b[0])) {
-                a[0] = {}
-                return shelf.remote_update(a, f, b)
-            }
-            if (is_obj(b[0])) {
-                a[0] = {}
-                shelf.merge(a, b)
-            } else a[0] = b[0]
-            f = shelf.read(a)
-        } else if (b[1] == a[1] && is_obj(a[0]) && is_obj(b[0])) {
-            if (is_obj(f)) {
-                for (let [k, v] of Object.entries(b[0])) {
-                    if (!a[0][k]) a[0][k] = [null, -1]
-                    f[k] = shelf.remote_update(a[0][k], f[k], v)
-                    if (f[k] == null) delete f[k]
-                }
-            } else shelf.merge(a, b)
-        }
-        return f
+shelf.to_braid = (s) => {
+  var vs = [];
+  var values = [];
+  function f(s) {
+    var x = s[0];
+    if (is_obj(x))
+      x = Object.fromEntries(Object.entries(x).map(([k, v]) => [k, f(v)]));
+    else {
+      values.push(x);
+      x = values.length - 1;
     }
+    vs.push(s[1]);
+    return x;
+  }
+  return {
+    json_slice: f(s),
+    values,
+    version: `${Math.random().toString(36).slice(2)}:${vs.join(",")}`,
+  };
+};
 
-    shelf.to_braid = s => {
-        var vs = []
-        var values = []
-        function f(s) {
-            var x = s[0]
-            if (is_obj(x)) x = Object.fromEntries(Object.entries(x).map(([k, v]) => [k, f(v)]))
-            else {
-                values.push(x)
-                x = values.length - 1
-            }
-            vs.push(s[1])
-            return x
-        }
-        return {json_slice: f(s), values, version: `${Math.random().toString(36).slice(2)}:${vs.join(',')}`}
-    }
+shelf.from_braid = ({ version, json_slice, values }) => {
+  var f = (x) => {
+    if (typeof x == "object")
+      return Object.fromEntries(Object.entries(x).map(([k, v]) => [k, f(v)]));
+    else return values[x];
+  };
+  var x = f(json_slice);
 
-    shelf.from_braid = ({version, json_slice, values}) => {
-        var f = x => {
-            if (typeof x == 'object') return Object.fromEntries(Object.entries(x).map(([k, v]) => [k, f(v)]))
-            else return values[x]
-        }
-        var x = f(json_slice)
+  var vs = version
+    .split(":")[1]
+    .split(",")
+    .map((x) => 1 * x);
+  f = (x) => {
+    return [
+      is_obj(x)
+        ? Object.fromEntries(Object.entries(x).map(([k, v]) => [k, f(v)]))
+        : x,
+      vs.shift(),
+    ];
+  };
+  return f(x);
+};
 
-        var vs = version.split(':')[1].split(',').map(x => 1*x)
-        f = x => {
-            return [is_obj(x) ? Object.fromEntries(Object.entries(x).map(([k, v]) => [k, f(v)])) : x, vs.shift()]
-        }
-        return f(x)
-    }
+function is_obj(o) {
+  return o && typeof o == "object" && !Array.isArray(o);
+}
 
-})()
+function greater_than(a, b) {
+  if (is_obj(b)) return false;
+  if (is_obj(a)) return true;
+  return JSON.stringify(a) > JSON.stringify(b);
+}
 
-if (typeof module != 'undefined') module.exports = shelf
+function equal(a, b) {
+  if (is_obj(a)) return is_obj(b);
+  if (is_obj(b)) return false;
+  return JSON.stringify(a) == JSON.stringify(b);
+}
--- test.js
+++ test.js
@@ -1,197 +1,209 @@
+import { shelf } from "./index.js";
 
-var shelf = require('./index.js')
+{
+  let a = [null, -1];
+  let b = [null, -1];
+  let a_msg = [null, -1];
+  let b_msg = [null, -1];
 
-;{
-    let a = [null, -1]
-    let b = [null, -1]
-    let a_msg = [null, -1]
-    let b_msg = [null, -1]
+  let n = 10000;
+  for (var i = 0; i < n; i++) {
+    if (i % 100 == 0) console.log(`${i}/${n}`);
 
-    let n = 10000
-    for (var i = 0; i < n; i++) {
-        if (i % 100 == 0) console.log(`${i}/${n}`)
-
-        if (!deep_eq(shelf.from_braid(shelf.to_braid(a)), a)) {
-            print(a)
-            print({to_braid: shelf.to_braid(a)})
-            print({from_braid: shelf.from_braid(shelf.to_braid(a))})
-            throw Error()
-        }
-
-        if (!looks_right(a)) throw Error()
-        if (!looks_right(b)) throw Error()
-        if (!looks_right(a_msg)) throw Error()
-        if (!looks_right(b_msg)) throw Error()
-
-        if (Math.random() < 0.5) {
-            let tk = tweek(shelf.read(a))
-            let m = shelf.merge(a, shelf.wrap(tk))
-            if (!m) throw Error()
-            shelf.merge(b_msg, m)
-        }
-
-        if (Math.random() < 0.5) {
-            let tk = tweek(shelf.read(b))
-            let m = shelf.merge(b, shelf.wrap(tk))
-            if (!m) throw Error()
-            shelf.merge(a_msg, m)
-        }
-
-        if (Math.random() < 0.1) {
-            shelf.merge(a, a_msg)
-            shelf.merge(b, b_msg)
-            a_msg = [null, -1]
-            b_msg = [null, -1]
-            if (!deep_eq(shelf.read(a), shelf.read(b))) throw Error()
-        }
+    if (!deep_eq(shelf.from_braid(shelf.to_braid(a)), a)) {
+      print(a);
+      print({ to_braid: shelf.to_braid(a) });
+      print({ from_braid: shelf.from_braid(shelf.to_braid(a)) });
+      throw Error();
     }
-}
 
-;{
-    let a = [null, -1]
-    let b = [null, -1]
-    let af = null
-    let bf = null
-    let a_msg = [null, -1]
-    let b_msg = [null, -1]
+    if (!looks_right(a)) throw Error();
+    if (!looks_right(b)) throw Error();
+    if (!looks_right(a_msg)) throw Error();
+    if (!looks_right(b_msg)) throw Error();
 
-    let n = 10000
-    for (var i = 0; i < n; i++) {
-        if (i % 100 == 0) console.log(`${i}/${n}`)
-
-        if (!looks_right(a)) throw Error()
-        if (!looks_right(b)) throw Error()
-        if (!looks_right(a_msg)) throw Error()
-        if (!looks_right(b_msg)) throw Error()
-
-        if (Math.random() < 0.2) af = tweek(af)
-
-        if (Math.random() < 0.2) bf = tweek(bf)
-
-        if (Math.random() < 0.2) {
-            let diff = shelf.local_update(a, af)
-            if (diff) shelf.merge(b_msg, diff)
-            if (!deep_eq(shelf.read(a), af)) throw Error()
-        }
-
-        if (Math.random() < 0.2) {
-            let diff = shelf.local_update(b, bf)
-            if (diff) shelf.merge(a_msg, diff)
-            if (!deep_eq(shelf.read(b), bf)) throw Error()
-        }
-
-        if (Math.random() < 0.1) {
-            af = shelf.remote_update(a, af, a_msg)
-            bf = shelf.remote_update(b, bf, b_msg)
-            a_msg = [null, -1]
-            b_msg = [null, -1]
-            if (!deep_eq(shelf.read(a), shelf.read(b))) throw Error()
-        }
-
-        if (Math.random() < 0.1) {
-            let diff = shelf.local_update(a, af)
-            if (diff) shelf.merge(b_msg, diff)
-
-            diff = shelf.local_update(b, bf)
-            if (diff) shelf.merge(a_msg, diff)
-
-            af = shelf.remote_update(a, af, a_msg)
-            bf = shelf.remote_update(b, bf, b_msg)
-            a_msg = [null, -1]
-            b_msg = [null, -1]
-
-            if (!deep_eq(shelf.read(a), shelf.read(b))) throw Error()
-            if (!deep_eq(shelf.read(a), af)) throw Error()
-            if (!deep_eq(shelf.read(b), bf)) throw Error()
-        }
+    if (Math.random() < 0.5) {
+      let tk = tweek(shelf.read(a));
+      let m = shelf.merge(a, shelf.wrap(tk));
+      if (!m) throw Error();
+      shelf.merge(b_msg, m);
     }
+
+    if (Math.random() < 0.5) {
+      let tk = tweek(shelf.read(b));
+      let m = shelf.merge(b, shelf.wrap(tk));
+      if (!m) throw Error();
+      shelf.merge(a_msg, m);
+    }
+
+    if (Math.random() < 0.1) {
+      shelf.merge(a, a_msg);
+      shelf.merge(b, b_msg);
+      a_msg = [null, -1];
+      b_msg = [null, -1];
+      if (!deep_eq(shelf.read(a), shelf.read(b))) throw Error();
+    }
+  }
 }
 
-;{
-    let a, b, f
-    a = [2, 2]
-    f = 3
-    b = [1, 1]
-    r = shelf.remote_update(a, f, b)
-    if (r != 3) throw 'bad'
-    if (JSON.stringify(a) != '[2,2]') throw 'bad'
+{
+  let a = [null, -1];
+  let b = [null, -1];
+  let af = null;
+  let bf = null;
+  let a_msg = [null, -1];
+  let b_msg = [null, -1];
 
-    a = [2, 2]
-    f = {a: 1, b: 2}
-    b = [{b: [1, 1]}, 3]
-    r = shelf.remote_update(a, f, b)
-    if (JSON.stringify(a) != '[{"b":[1,1]},3]') throw 'bad'
-    if (JSON.stringify(r) != '{"a":1,"b":1}') throw 'bad'
+  let n = 10000;
+  for (var i = 0; i < n; i++) {
+    if (i % 100 == 0) console.log(`${i}/${n}`);
+
+    if (!looks_right(a)) throw Error();
+    if (!looks_right(b)) throw Error();
+    if (!looks_right(a_msg)) throw Error();
+    if (!looks_right(b_msg)) throw Error();
+
+    if (Math.random() < 0.2) af = tweek(af);
+
+    if (Math.random() < 0.2) bf = tweek(bf);
+
+    if (Math.random() < 0.2) {
+      let diff = shelf.local_update(a, af);
+      if (diff) shelf.merge(b_msg, diff);
+      if (!deep_eq(shelf.read(a), af)) throw Error();
+    }
+
+    if (Math.random() < 0.2) {
+      let diff = shelf.local_update(b, bf);
+      if (diff) shelf.merge(a_msg, diff);
+      if (!deep_eq(shelf.read(b), bf)) throw Error();
+    }
+
+    if (Math.random() < 0.1) {
+      af = shelf.remote_update(a, af, a_msg);
+      bf = shelf.remote_update(b, bf, b_msg);
+      a_msg = [null, -1];
+      b_msg = [null, -1];
+      if (!deep_eq(shelf.read(a), shelf.read(b))) throw Error();
+    }
+
+    if (Math.random() < 0.1) {
+      let diff = shelf.local_update(a, af);
+      if (diff) shelf.merge(b_msg, diff);
+
+      diff = shelf.local_update(b, bf);
+      if (diff) shelf.merge(a_msg, diff);
+
+      af = shelf.remote_update(a, af, a_msg);
+      bf = shelf.remote_update(b, bf, b_msg);
+      a_msg = [null, -1];
+      b_msg = [null, -1];
+
+      if (!deep_eq(shelf.read(a), shelf.read(b))) throw Error();
+      if (!deep_eq(shelf.read(a), af)) throw Error();
+      if (!deep_eq(shelf.read(b), bf)) throw Error();
+    }
+  }
 }
 
-console.log('passed!')
+{
+  let a, b, f, r;
+  a = [2, 2];
+  f = 3;
+  b = [1, 1];
+  r = shelf.remote_update(a, f, b);
+  if (r != 3) throw "bad";
+  if (JSON.stringify(a) != "[2,2]") throw "bad";
 
-function print(...a) { console.log(...a.map(x => JSON.stringify(x, null, '    '))) }
+  a = [2, 2];
+  f = { a: 1, b: 2 };
+  b = [{ b: [1, 1] }, 3];
+  r = shelf.remote_update(a, f, b);
+  if (JSON.stringify(a) != '[{"b":[1,1]},3]') throw "bad";
+  if (JSON.stringify(r) != '{"a":1,"b":1}') throw "bad";
+}
 
-function clone(o) { return JSON.parse(JSON.stringify(o)) }
+console.log("passed!");
 
-function is_obj(o) { return o && typeof o == 'object' && !Array.isArray(o) }
+function print(...a) {
+  console.log(...a.map((x) => JSON.stringify(x, null, "    ")));
+}
+
+function clone(o) {
+  return JSON.parse(JSON.stringify(o));
+}
+
+function is_obj(o) {
+  return o && typeof o == "object" && !Array.isArray(o);
+}
 function equal(a, b) {
-    if (is_obj(a)) return is_obj(b)
-    if (is_obj(b)) return false
-    return JSON.stringify(a) == JSON.stringify(b)
+  if (is_obj(a)) return is_obj(b);
+  if (is_obj(b)) return false;
+  return JSON.stringify(a) == JSON.stringify(b);
 }
 
 function looks_right(s) {
-    if (!Array.isArray(s)) return false
-    if (s.length < 1 || s.length > 2) return false
-    if (!Number.isInteger(s[1]) && s != null && s != 'add') return false
-    return is_obj(s[0]) ? Object.entries(s[0]).every(([k, v]) => looks_right(v)) : true
+  if (!Array.isArray(s)) return false;
+  if (s.length < 1 || s.length > 2) return false;
+  if (!Number.isInteger(s[1]) && s != null && s != "add") return false;
+  return is_obj(s[0])
+    ? Object.entries(s[0]).every(([k, v]) => looks_right(v))
+    : true;
 }
 
 function deep_eq(a, b) {
-    if (a && typeof a == 'object' && b && typeof b == 'object') {
-        return Object.entries(a).every(([k, v]) => deep_eq(v, b[k])) && Object.entries(b).every(([k, v]) => deep_eq(v, a[k]))
-    } else return a == b    
+  if (a && typeof a == "object" && b && typeof b == "object") {
+    return (
+      Object.entries(a).every(([k, v]) => deep_eq(v, b[k])) &&
+      Object.entries(b).every(([k, v]) => deep_eq(v, a[k]))
+    );
+  } else return a == b;
 }
 
 function create_random_string() {
-    return String.fromCharCode('a'.charCodeAt(0) + Math.floor(Math.random() * 26)).repeat(Math.floor(Math.random() * 3) + 1)
+  return String.fromCharCode(
+    "a".charCodeAt(0) + Math.floor(Math.random() * 26)
+  ).repeat(Math.floor(Math.random() * 3) + 1);
 }
 
 function create_random_value() {
-    if (Math.random() < 0.2) {
-        let x = {}
-        let n = Math.floor(Math.random() * 4)
-        for (let i = 0; i < n; i++) {
-            let k = create_random_string()
-            x[k] = create_random_value()
-        }
-        return x
-    } else if (Math.random() < 0.25) return Math.floor(Math.random() * 100)
-    else if (Math.random() < 0.333) return create_random_string()
-    else if (Math.random() < 0.5) return Math.random() < 0.5
-    else if (Math.random() < 0.5) return null
-    else {
-        let a = []
-        let n = Math.floor(Math.random() * 4)
-        for (let i = 0; i < n; i++) a.push(create_random_value())
-        return a
+  if (Math.random() < 0.2) {
+    let x = {};
+    let n = Math.floor(Math.random() * 4);
+    for (let i = 0; i < n; i++) {
+      let k = create_random_string();
+      x[k] = create_random_value();
     }
+    return x;
+  } else if (Math.random() < 0.25) return Math.floor(Math.random() * 100);
+  else if (Math.random() < 0.333) return create_random_string();
+  else if (Math.random() < 0.5) return Math.random() < 0.5;
+  else if (Math.random() < 0.5) return null;
+  else {
+    let a = [];
+    let n = Math.floor(Math.random() * 4);
+    for (let i = 0; i < n; i++) a.push(create_random_value());
+    return a;
+  }
 }
 
 function pick(x) {
-    return x[Math.floor(Math.random() * x.length)]
+  return x[Math.floor(Math.random() * x.length)];
 }
 
 function tweek(x) {
-    if (is_obj(x)) {
-        if (Math.random() < 0.2) {
-            return null
-        } else {
-            let k = Math.random() < 0.8 && pick(Object.keys(x))
-            if (!k) k = create_random_string()
-            x[k] = tweek(x[k] ?? null)
-            return x
-        }
+  if (is_obj(x)) {
+    if (Math.random() < 0.2) {
+      return null;
     } else {
-        let new_val = x
-        while (equal(new_val, x)) new_val = create_random_value()
-        return new_val
+      let k = Math.random() < 0.8 && pick(Object.keys(x));
+      if (!k) k = create_random_string();
+      x[k] = tweek(x[k] ?? null);
+      return x;
     }
+  } else {
+    let new_val = x;
+    while (equal(new_val, x)) new_val = create_random_value();
+    return new_val;
+  }
 }
