3b36af1ed803e1e4e33f46e112f9f53964f970d6
Duane Johnson <duane.johnson@gmail.com>
2023-06-10 16:05:07 -0600

    Remove shelf.get, "add" sentinel

--- index.js
+++ index.js
@@ -21,7 +21,6 @@ shelf.read = (s, ...path) => {
     );
   } else return s?.[0];
 };
-shelf.get = shelf.read;
 
 shelf.get_change = (a, b) => {
   return shelf.merge(a, b, true);
@@ -37,7 +36,6 @@ shelf.merge = (a, b, dont_modify) => {
   let eq = equal(a[0], b[0]);
 
   if (b[1] == null) b = [b[0], a[1] + (eq ? 0 : 1)];
-  else if (b[1] == "add") b = [a[0] + b[0], a[1] + 1];
 
   if (b[1] > (a[1] ?? -1) || (b[1] == a[1] && greater_than(b[0], a[0]))) {
     if (is_obj(b[0])) {
